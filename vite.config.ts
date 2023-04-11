a1bfc140f9eb7d691b64c26bab3ec7514f74e79e
pedromacedol <pedromacedoldev@gmail.com>
2023-04-11 18:11:12 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "tractian-challenge-swe-frontend.vercel.app/",
   plugins: [react()],
   build: {
     outDir: "build",
